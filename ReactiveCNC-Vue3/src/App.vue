3d92e7ee5d5c054434f5c90c80a8ffa8e2d83089
Tinic Uro <turo@adobe.com>
2023-04-20 16:13:52 -0700

    Experiment in progress.

--- ReactiveCNC-Vue3/src/App.vue
+++ ReactiveCNC-Vue3/src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import GCodeTable from './components/GCodeTable.vue';
 </script>
 
 <template>
-There is nothing here
+  <GCodeTable style="width:600px;"></GCodeTable>
+  This is just a test.
 </template>
 
 <style scoped>
+.wrapper,
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
 </style>
--- ReactiveCNC-Vue3/src/main.ts
+++ ReactiveCNC-Vue3/src/main.ts
@@ -1,5 +1,42 @@
-import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createWebHistory, createRouter } from "vue-router";
+
+import './style.css'
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/md-dark-indigo/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import DialogService from 'primevue/dialogservice';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';
+import Row from 'primevue/row';
+import Checkbox from 'primevue/checkbox';
+
+const routes = [{
+  path: "/",
+  name: "Home",
+  component: App,
+}];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(DialogService);
+app.use(router)
+
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
+app.component('Checkbox', Checkbox);
+
+app.mount('#app')
