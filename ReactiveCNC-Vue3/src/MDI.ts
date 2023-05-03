4d5785ebb496080448ce15ac27bf1e6020c66a3b
Tinic Uro <tinicuro@gmail.com>
2023-05-03 14:37:14 -0700

    New files.

---
+++ ReactiveCNC-Vue3/src/MDI.ts
@@ -0,0 +1,31 @@
+import 'highlightjs/styles/github.css';
+import hljs from 'highlightjs';
+import { ref } from "vue";
+
+function insertSpaces(str:string):string {
+    const segments = str.split(/(\([^)]*\))/g);
+    for (let i = 0; i < segments.length; i++) {
+        if (i % 2 === 0) {
+            segments[i] = segments[i].replace(/([A-Z])(?![a-z])/g, " $1");
+        }
+    }
+    return segments.join("");
+}
+
+export class MDILine {
+    public gcode:string = '';
+    public highlight:string = '';
+    public uuid:string = crypto.randomUUID();
+
+    constructor(gcode:string) {
+        this.gcode = gcode;
+        this.highlight = hljs.highlight("gcode", insertSpaces(gcode)).value;
+    }
+};
+
+export class MDI {
+    lines:Array<MDILine> = new Array<MDILine>();
+}
+
+export var mdi = new MDI();
+export const mdiLinesRef = ref<Array<MDILine>>(mdi.lines);
