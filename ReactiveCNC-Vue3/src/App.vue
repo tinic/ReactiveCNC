9187ebb45d67e0e77b77cbc9f773ccf52c412166
Tinic Uro <turo@adobe.com>
2023-05-04 16:51:33 -0700

    WIP

--- ReactiveCNC-Vue3/src/App.vue
+++ ReactiveCNC-Vue3/src/App.vue
@@ -1,71 +1,16 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import { loadGCodeFromURL } from "./GCode.ts";
-import GCodeTable from "./components/GCodeTable.vue";
-import MDITable from "./components/MDITable.vue";
-import JogPanel from "./components/JogPanel.vue";
-import { ref, onMounted } from "vue";
-
-const gcodeTable = ref();
+import ModePanels from "./components/ModePanels.vue";
 
 onMounted(() => {
   loadGCodeFromURL("/src/assets/gcode.txt");
 });
 
-const jogPanelRef = ref();
-
-const activeIndex = ref(0);
-const onActiveIndex = () => {
-  console.log("onActiveIndex " + activeIndex.value);
-  switch (activeIndex.value) {
-    case 0:
-      jogPanelRef.value.removeKeyboardHandler();
-      break;
-    case 1:
-      jogPanelRef.value.removeKeyboardHandler();
-      break;
-    case 2:
-      jogPanelRef.value.installKeyboardHandler();
-      break;
-  }
-};
 </script>
 
 <template>
-  <TabView
-    v-model:activeIndex="activeIndex"
-    @update:activeIndex="onActiveIndex"
-  >
-    <TabPanel>
-      <template #header>
-        <i class="pi pi-cog m-0 p-0 pr-2" style="font-size: 1.5rem" />
-        <span>PROGRAM</span>
-      </template>
-      <GCodeTable
-        ref="gcodeTable"
-        :disabled="false"
-        style="width: 600px; height: 25em"
-      >
-      </GCodeTable>
-    </TabPanel>
-    <TabPanel>
-      <template #header>
-        <i class="pi pi-calculator m-0 p-0 pr-2" style="font-size: 1.5rem" />
-        <span>MDI</span>
-      </template>
-      <div style="width: 600px; height: 25em">
-        <MDITable> </MDITable>
-      </div>
-    </TabPanel>
-    <TabPanel>
-      <template #header>
-        <i class="pi pi-arrows-alt m-0 p-0 pr-2" style="font-size: 1.5rem" />
-        <span>JOG</span>
-      </template>
-      <div style="width: 600px; height: 25em">
-        <JogPanel ref="jogPanelRef"> </JogPanel>
-      </div>
-    </TabPanel>
-  </TabView>
+  <ModePanels></ModePanels>
 </template>
 
 <style>
@@ -75,17 +20,6 @@ body {
   width: 100%;
   height: 100%;
   margin: 0;
-}
-
-#app .p-tabview-header {
-  font-weight: regular;
-  font-size: 1.5em;
-  padding: 0;
-  margin: 0;
-}
-
-#app .p-tabview .p-tabview-panels {
-  padding: 0em;
 }
 
 .wrapper {
---
+++ ReactiveCNC-Vue3/src/components/ModePanels.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import GCodePanel from "./GCodePanel.vue";
+import MDIPanel from "./MDIPanel.vue";
+import JogPanel from "./JogPanel.vue";
+import { ref } from "vue";
+
+const gcodeTable = ref();
+const jogPanelRef = ref();
+const activeIndex = ref(0);
+
+const onActiveIndex = () => {
+  console.log("onActiveIndex " + activeIndex.value);
+  switch (activeIndex.value) {
+    case 0:
+      jogPanelRef.value.removeKeyboardHandler();
+      break;
+    case 1:
+      jogPanelRef.value.removeKeyboardHandler();
+      break;
+    case 2:
+      jogPanelRef.value.installKeyboardHandler();
+      break;
+  }
+};
+</script>
+
+<template>
+  <TabView
+    v-model:activeIndex="activeIndex"
+    @update:activeIndex="onActiveIndex"
+  >
+    <TabPanel>
+      <template #header>
+        <i class="pi pi-cog m-0 p-0 pr-2" style="font-size: 1.5rem" />
+        <span>PROGRAM</span>
+      </template>
+      <GCodePanel
+        ref="gcodeTable"
+        :disabled="false"
+        style="width: 600px; height: 25em"
+      >
+      </GCodePanel>
+    </TabPanel>
+    <TabPanel>
+      <template #header>
+        <i class="pi pi-calculator m-0 p-0 pr-2" style="font-size: 1.5rem" />
+        <span>MDI</span>
+      </template>
+      <div style="width: 600px; height: 25em">
+        <MDIPanel> </MDIPanel>
+      </div>
+    </TabPanel>
+    <TabPanel>
+      <template #header>
+        <i class="pi pi-arrows-alt m-0 p-0 pr-2" style="font-size: 1.5rem" />
+        <span>JOG</span>
+      </template>
+      <div style="width: 600px; height: 25em">
+        <JogPanel ref="jogPanelRef"> </JogPanel>
+      </div>
+    </TabPanel>
+  </TabView>
+</template>
+
+<style>
+#app .p-tabview-header {
+  font-weight: regular;
+  font-size: 1.5em;
+  padding: 0;
+  margin: 0;
+}
+
+#app .p-tabview .p-tabview-panels {
+  padding: 0em;
+}
+</style>
