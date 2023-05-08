5ddfccd4fc4f946f4675cd1769e0560e030d3326
Tinic Uro <turo@adobe.com>
2023-05-08 15:07:57 -0700

    Add missing file.

---
+++ ReactiveCNC-Vue3/src/components/DefaultPanel.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+export interface Props {
+  disabled: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  disabled: false,
+});
+</script>
+
+<template>
+  <div :class="{ disabled: props.disabled }">
+    This panel is intentially left blank
+  </div>
+</template>
